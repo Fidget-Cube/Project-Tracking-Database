942b5b31e7208046fd9399485ed867d8da5a807f
APstarwars <hobbes399@gmail.com>
2020-11-02 21:10:39 -0800

    Add Request Handler

--- PTD.js
+++ PTD.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 const express = require('express');
 const fs = require('fs');
+const url = require('url');
 
 const app = express();
 const port = 8080;
 
-var con = mysql.createConnection({
+const con = mysql.createConnection({
 	host: "localhost",
 	user: "mvonblan_cs355fl20",
 	password: "vo7615745"
@@ -16,10 +17,20 @@ con.connect(function(err) {
 	console.log("Connected!");
 });
 
-app.use(express.static('public'));
-
 app.get('/', (req, res) => {
 	res.redirect('/home.html');
 });
+
+app.get('/view.html', (req, res) => {
+	const q = url.parse(req.url, true).query;
+	const txt = q.fname;
+	if (txt) {
+		res.send(txt);
+	} else {
+		res.sendFile('/home/student/vonblanken/Project-Tracking-Database/public/view.html');
+	}
+});
+
+app.use(express.static('public'));
 
 app.listen(port, () => {console.log(`Listening on http://blue.cs.sonoma.edu:${port}/`)});
