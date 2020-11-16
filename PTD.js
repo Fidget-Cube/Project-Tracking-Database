42686d7981197c60721a80d7f082311a88369d24
APstarwars <hobbes399@gmail.com>
2020-11-16 14:29:05 -0800

    Add Data to Query

--- PTD.js
+++ PTD.js
@@ -3,7 +3,10 @@
 const express = require('express');
 const url = require('url');
 const customerController = new(require('./control/customerController.js'))();
+const locationController = new(require('./control/locationController.js'))();
 const projectController = new(require('./control/projectController.js'))();
+const installationController = new(require('./control/installationController.js'))();
+const orderController = new(require('./control/orderController.js'))();
 
 const app = express();
 const port = 8080;
@@ -19,6 +22,10 @@ app.get('/view.html', async function(req, res) {
     if (q.project) {
         let project = await projectController.getProjectByID(q.project);
         if (project) {
+            let installation = await installationController.getInstallationByProject(project[0].projectID);
+            if (installation) project = project.concat(installation);
+            let order = await orderController.getOrderByProject(project[0].projectID);
+            if (order) project = project.concat(order);
             res.send(project);
         } else {
             res.send("Error fetching data, check your input.");
@@ -26,6 +33,8 @@ app.get('/view.html', async function(req, res) {
     } else if (q.customer) {
         let customer = await customerController.getCustomerByName(q.customer);
         if (customer) {
+            let location = await locationController.getLocationByCustomer(customer[0].customerID);
+            if (location) customer = customer.concat(location);
             res.send(customer);
         } else {
             res.send("Error fetching data, check your input.");
